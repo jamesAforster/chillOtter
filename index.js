2a87d95d5a24da2a92dffa9816cd67ede36d95f1
jamesAforster <jamesalexforster@gmail.com>
2020-10-04 12:35:46 +0100

    fixes env variables

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-require('dotenv').config()
-const Twit = require('twit')
+require('dotenv').config();
+const Twit = require('twit');
 
 var T = new Twit ({
-  consumer_key: TWIT_CONSUMER_KEY,
-  consumer_secret: TWIT_CONSUMER_SECRET,
-  access_token: TWIT_ACCESS_TOKEN,
-  access_token_secret: TWIT_ACCESS_ROKEN_SECRET
-})
+  consumer_key: process.env.TWIT_CONSUMER_KEY,
+  consumer_secret: process.env.TWIT_CONSUMER_SECRET,
+  access_token: process.env.TWIT_ACCESS_TOKEN,
+  access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
+});
 
 T.post('statuses/update', { status: "🦦" }, function(err, data, response ) {
   console.log(data)
-})
-
+});
