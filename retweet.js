8714d5547fd39f80440b9c34de5ed9c5abf215f7
jamesAforster <jamesalexforster@gmail.com>
2020-10-13 19:34:55 +0100

    removes retweet.js from gitignore. etweet.js will now reply to tweet at random

---
+++ retweet.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const Twit = require('twit');
+
+var T = new Twit ({
+  consumer_key: process.env.TWIT_CONSUMER_KEY,
+  consumer_secret: process.env.TWIT_CONSUMER_SECRET,
+  access_token: process.env.TWIT_ACCESS_TOKEN,
+  access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
+});
+
+
+replyToTweet = () => { 
+  T.get('search/tweets', { q: 'otter', count: 20 }, function(err, data, response) {
+    tweetData = data.statuses[5]
+    postReply(tweetData)
+  })
+}
+
+// I want to define a variable inside findTweet called replyTweet, then I can access the properties of that for the
+// replyToTweet function where I need user.id_str
+// generateString() where I need user.screen_name
+
+postReply = (tweetData) => { 
+  T.post('statuses/update', { status: generateReply(tweetData), in_reply_to_status_id: tweetData.id_str }, function(err, data, response) {
+    console.log(data)
+  })
+}
+
+var tweet = [
+  [["🌾"],["🌾"],["🌾"],["🌾"],["🌾"],["🌾"],["🌾"], ["🌾"], ["🌾"] ,["🌾"]],
+  [["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"]],
+  [["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"]],
+  [["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"]],
+  [["🌾"],["🌾"],["🌾"],["🌾"],["🌾"],["🌾"],["🌾"], ["🌾"], ["🌾"] ,["🌾"]],
+];
+
+var generateReply = (replyTweet) => {
+  insertOtter();
+  let string = ""
+  tweet.forEach(e => { 
+    string += e.join('')  
+    string += "\n"
+  });
+  return string + `\n\n did someone say otter? @${replyTweet.user.screen_name}`;
+};
+
+var insertOtter = () => {
+  coordinates = getRandomArrayCoordinates()
+  tweet[coordinates.x][coordinates.y] = "🦦";
+  tweet[x].pop();
+};
+
+var getRandomArrayCoordinates = () => {
+  x = Math.floor(Math.random() * Math.floor(3) +1);
+  y = Math.floor(Math.random() * Math.floor(18));
+  return {x: x, y: y}
+}
+
+replyToTweet()
